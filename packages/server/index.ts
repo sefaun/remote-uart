b0c51dbd8622b7a00b42a7a97622571ed2e9d006
Sefa UN <sefaun6654@hotmail.com>
2025-04-06 04:54:50 +0300

    feature: server eklendi.

---
+++ packages/server/index.ts
@@ -0,0 +1,21 @@
+import { createServer } from 'node:net'
+import Aedes from 'aedes'
+const port = 8883
+
+// @ts-ignore
+const aedes = new Aedes()
+const server = createServer(aedes.handle)
+
+server.listen(port, function () {
+  console.log('MQTT server listening on port ', port)
+
+  // @ts-ignore
+  aedes.on('publish', function (packet, _client) {
+    console.log(console.log('publish packet:', packet.payload))
+  })
+
+  // @ts-ignore
+  aedes.on('client', function (client) {
+    console.log('new client', client.id)
+  })
+})
